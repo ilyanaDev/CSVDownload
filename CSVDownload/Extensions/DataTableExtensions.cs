c70911426534b87bfed9218ff5bafcc8f2d5a53a
Ilyana Smith <ilyana@kentsmiths.com>
2021-04-29 14:11:49 -0400

    refactoring

---
+++ CSVDownload/Extensions/DataTableExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.IO;
+
+namespace CSVDownload.Extensions
+{
+    public static class DataTableExtensions
+    {
+        public static byte[] ToCsvByteArray(this DataTable input)
+        {
+            var stream = new MemoryStream();
+            StreamWriter sw = new StreamWriter(stream);
+
+            for (int i = 0; i < input.Columns.Count; i++)
+            {
+                sw.Write(input.Columns[i]);
+                if (i < input.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow row in input.Rows)
+            {
+                for (int i = 0; i < input.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(row[i]))
+                    {
+                        string value = row[i].ToString();
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(row[i].ToString());
+                        }
+                    }
+                    if (i < input.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+
+            return stream.ToArray();
+
+        }
+    }
+}
--- CSVDownload/Pages/Index.cshtml.cs
+++ CSVDownload/Pages/Index.cshtml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Data;
-using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CSVDownload.Extensions;
 
 namespace CSVDownload.Pages
 {
@@ -25,53 +25,6 @@ namespace CSVDownload.Pages
             {
                 FileDownloadName = filePath
             };
-
-        }
-    }
-
-    public static class DataTableExtensions
-    {
-        public static byte[] ToCsvByteArray(this DataTable input)
-        {
-            var stream = new MemoryStream();
-            StreamWriter sw = new StreamWriter(stream);
-
-            for (int i = 0; i < input.Columns.Count; i++)
-            {
-                sw.Write(input.Columns[i]);
-                if (i < input.Columns.Count - 1)
-                {
-                    sw.Write(",");
-                }
-            }
-            sw.Write(sw.NewLine);
-            foreach (DataRow row in input.Rows)
-            {
-                for (int i = 0; i < input.Columns.Count; i++)
-                {
-                    if (!Convert.IsDBNull(row[i]))
-                    {
-                        string value = row[i].ToString();
-                        if (value.Contains(','))
-                        {
-                            value = String.Format("\"{0}\"", value);
-                            sw.Write(value);
-                        }
-                        else
-                        {
-                            sw.Write(row[i].ToString());
-                        }
-                    }
-                    if (i < input.Columns.Count - 1)
-                    {
-                        sw.Write(",");
-                    }
-                }
-                sw.Write(sw.NewLine);
-            }
-            sw.Close();
-
-            return stream.ToArray();
 
         }
     }
