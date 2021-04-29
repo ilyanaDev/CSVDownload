81487ccd42655be799cbebeb006da748211a8598
Ilyana Smith <ilyana@kentsmiths.com>
2021-04-29 14:42:41 -0400

    changing data table

--- CSVDownload/Pages/CsvCreator.cs
+++ CSVDownload/Pages/CsvCreator.cs
@@ -1,14 +1,4 @@
-﻿using CSVDownload.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text;
+﻿using System.Data;
 
 namespace CSVDownload.Pages
 {
@@ -23,7 +13,7 @@ namespace CSVDownload.Pages
             myDataTable.Columns.Add("TITLE", typeof(string));
             myDataTable.Columns.Add("AUTHOR", typeof(string));
 
-            myDataTable.Rows.Add("2020-12-10", "Introduction to GUIDs", "Ilyana");
+            myDataTable.Rows.Add("2020-12-29", "Gatsby Shadowing", "Ilyana");
             myDataTable.Rows.Add("2021-02-23", "This is a blog post", "Joe");
             myDataTable.Rows.Add("1957-03-05", "There and back again", "Bilbo Baggins");
             myDataTable.Rows.Add("2000-01-01", "That's no moon", "Obi-Wan Kenobi");
